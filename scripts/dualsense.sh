547c74ff6d4de5f7a5d0dafd9ba1c80122aafef1
BonjeBongo <37078653+BonjeBongo@users.noreply.github.com>
2025-12-12 03:03:02 -0500

    sync: turn off vrr, oled flicker

--- scripts/dualsense.sh
+++ scripts/dualsense.sh
@@ -1,20 +1,14 @@
-cat << EOF > /etc/udev/rules.d/90-dualsense-touchpad.rules
-# PS5 DualSense controller over USB hidraw
-KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
+cat << EOF > /usr/lib/udev/rules.d/71-sony-controllers-joymouse.rules
+# Disable DS4 touchpad acting as mouse
+# USB
+ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+# Bluetooth
+ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
 
-# PS5 DualSense controller over bluetooth hidraw
-KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
-
-# PS5 DualSense Edge controller over USB hidraw
-KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0df2", MODE="0660", TAG+="uaccess"
-
-# PS5 DualSense Edge controller over bluetooth hidraw
-KERNEL=="hidraw*", KERNELS=="*054C:0DF2*", MODE="0660", TAG+="uaccess"
-
-# Disable touchpad
+# Disable DualSense touchpad acting as mouse
 # USB
 ATTRS{name}=="Sony Interactive Entertainment DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-# BT
+# Bluetooth
 ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
 
 # Disable DualSense Edge touchpad acting as mouse
@@ -23,6 +17,6 @@ ATTRS{name}=="Sony Interactive Entertainment DualSense Edge Wireless Controller 
 ATTRS{name}=="DualSense Edge Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
 EOF
 
-cat << EOF > /etc/modules-load.d/uinput.conf
-uinput
-EOF
+# cat << EOF > /etc/modules-load.d/uinput.conf
+# uinput
+# EOF
