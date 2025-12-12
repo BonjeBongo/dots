ecb1dd56b644498ef6b6d48a184ab9cf3d17fa2a
BonjeBongo <37078653+BonjeBongo@users.noreply.github.com>
2025-12-12 03:20:14 -0500

    feat: fix efi for win update

--- scripts/sec-boot.sh
+++ scripts/sec-boot.sh
@@ -1,14 +1,26 @@
-WIN_BOOT=$(lsblk | grep 99M | grep -oP '(?<=─)[a-zA-Z0-9]+')
-mkdir -p /mnt/windows-efi/
-mount /dev/$WIN_BOOT /mnt/windows-efi/
-cp -r EFI/Microsoft /boot/EFI/
+cp /usr/share/edk2-shell/x64/Shell.efi /boot/shellx64.efi
+cat << EOF | tee -a /boot/loader/loader.conf
+console-mode max
+EOF
+# grab windows PARTUUID:
+# sudo blkid /dev/nvme1n1p3
+# boot to edk2-shell
+# find uuid
+# copy FS ALIAS
+mkdir -p /boot/loader/entries
+cat << EOF | tee /boot/loader/entries/windows.conf
+title   Windows
+efi     /shellx64.efi
+options -nointerrupt -nomap -noversion <REPLACEME>:EFI\Microsoft\Boot\Bootmgfw.efi
+EOF
+
 
 sbctl create-keys
 sbctl enroll-keys --microsoft
 
-
 cat << EOF | tee /etc/dracut.conf.d/myflags.conf
 add_dracutmodules+=" plymouth "
+force_drivers+=" amdgpu "
 EOF
 sbctl verify | sed -E 's|^.* (/.+) is not signed$|sbctl sign -s "\1"|e'
 dracut-ukify -a
