7d258db4a82a2d9adcaf025b1ceafc85a3075845
BonjeBongo <37078653+BonjeBongo@users.noreply.github.com>
2025-12-15 01:03:28 -0500

    fix: sound device names

--- scripts/hosts/IceDevice.sh
+++ scripts/hosts/IceDevice.sh
@@ -5,4 +5,9 @@ efi     /shellx64.efi
 options -nointerrupt -nomap -noversion HD1d:EFI\Microsoft\Boot\Bootmgfw.efi
 EOF
 
+ACTUAL_USER="${SUDO_USER:-$USER}"
+ACTUAL_HOME=$(sudo -i -u $ACTUAL_USER echo "\$HOME")
+DCLI_DOTS='.config/arch-config/dots'
 
+ln -s "$ACTUAL_HOME/$DCLI_DOTS/hosts/IceDevice/wireplumber" "$ACTUAL_HOME/.config/wireplumber"
+systemctl --user restart wireplumber.service
