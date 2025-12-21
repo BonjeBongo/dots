25c46b225dcd0c3cde9f958ae74928f4ae3b17b1
BonjeBongo <37078653+BonjeBongo@users.noreply.github.com>
2025-12-21 03:14:59 -0500

    fix: a bunch

--- scripts/hosts/IceDevice.sh
+++ scripts/hosts/IceDevice.sh
@@ -5,9 +5,30 @@ efi     /shellx64.efi
 options -nointerrupt -nomap -noversion HD1d:EFI\Microsoft\Boot\Bootmgfw.efi
 EOF
 
+# sbctl hook crashes with versioned ukify kernel images so this hook should
+# happen before the one shipped with aur and clean up old versions
+cat << EOF | tee /usr/lib/kernel/install.d/91-pre-sbctl.install
+# remove invalid signs
+sbctl verify | sed -E 's|^.* (/.+): open |sbctl remove-file "\1"|e'
+# sign again
+sbctl verify | sed -E 's|^.* (/.+) is not signed$|sbctl sign -s "\1"|e'
+EOF
+
+cat << EOF | tee -a /etc/sddm.conf
+[General]
+DisplayServer=x11
+
+[X11]
+EnableHiDPI=true
+DisplayCommand=/usr/share/sddm/scripts/Xsetup
+DisplayStopCommand=/usr/share/sddm/scripts/Xstop
+
+EOF
+
 ACTUAL_USER="${SUDO_USER:-$USER}"
 ACTUAL_HOME=$(sudo -i -u $ACTUAL_USER echo "\$HOME")
 DCLI_DOTS='.config/arch-config/dots'
 
-ln -s "$ACTUAL_HOME/$DCLI_DOTS/hosts/IceDevice/wireplumber" "$ACTUAL_HOME/.config/wireplumber"
+ln -s "$ACTUAL_HOME/$DCLI_DOTS/hosts/IceDevice/wireplumber" \
+  "$ACTUAL_HOME/.config/wireplumber"
 systemctl --user restart wireplumber.service
--- scripts/sddm.sh
+++ scripts/sddm.sh
@@ -1,7 +1,15 @@
-sudo git clone -b master --depth 1 https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
+sudo git clone -b master --depth 1 \
+  https://github.com/keyitdev/sddm-astronaut-theme.git \
+  /usr/share/sddm/themes/sddm-astronaut-theme
 sudo cp -r /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
-echo "[Theme]
-Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
+
+cat << EOF | tee /etc/sddm.conf
+[Theme]
+Current=sddm-astronaut-theme
+EOF
+
 sudo mkdir -p /etc/sddm.conf.d
-echo "[General]
-InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
+cat << EOF | tee/etc/sddm.conf.d/virtualkbd.conf
+[General]
+InputMethod=qtvirtualkeyboard
+EOF
